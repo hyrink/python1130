89b5186c13d20ac62b1cfce1856b879cb399ea85
hyrink <simduddls@naver.com>
2021-11-30 16:49:48 +0900

    1130 오후 연습 마침

---
+++ Function1.py
@@ -0,0 +1,37 @@
+# Function1.py
+# 함수를 정의
+
+def setValue(newValue):
+    # 함수 내부에서 초기화 하면 지역변수(Local Variable)
+    x = newValue
+    print("함수내부:",x)
+
+# 함수를 호출 (리턴이 없다 ==> None)
+result = setValue(5)
+print(result)
+
+# 다중의 값을 리턴
+def swap(x,y):
+    return y,x
+
+# 호출
+result = swap(5,6)
+print(result)
+print(result[0],result[1])
+
+# 디버깅연습(교집합 문자를 리턴)
+def union(prelist, postlist):
+    # 교집합 문자를 저장하기 위한 지역 변수
+    result = []
+    # H(0) | A(2) | M(2)
+    for x in prelist:
+        # x라는 글자가 postlist에 있고, x가 result에 없으면
+        if x in postlist and x not in result:
+            # 리스트형식에 추가
+            result.append(x)
+    return result
+
+# 호출
+print(union("HAM","SPAM"))
+
+
---
+++ Funtion3.py
@@ -0,0 +1,58 @@
+# Funtion3.py
+# 함수의 기본값이 있는 경우
+def times(a=10, b=20):
+    return a*b
+
+# 호출
+print(times())
+print(times(5))
+print(times(5,6))
+
+# 키워드 인자
+def userURI(server,port):
+    strURL = "http://" + server + ":" + port
+    return strURL
+
+# 호출
+print(userURI("credu.com","80"))
+print(userURI(port="8080", server="credu.com"))
+
+# 가변인자는 입력되는 인자의 갯수가 가변적인 경우
+# 인자에 *를 붙이면 가변인자(내부에서 Tuple)
+def union(*ar):
+    # 지역변수로 결과를 담을 리스트를 초기화
+    result = []
+    # HAM(0) | EGG(1) ==> ar이라는 가변인자
+    for item in ar:
+        # H(0) | A(1) | M(2)
+        # E(0) | G(1) | G(2)
+        for x in item:
+            # x라는 글자가 포함되어 있지 않다면 추가
+            if x not in result:
+                result.append(x)
+    return result
+
+# 호출 (중단점, Break Point)
+print(union("HAM","EGG"))
+print(union("HAM","EGG","SPAM"))
+
+# 정의되지 않은 인자 처리(필수, 옵션도 섞여 있는 경우)
+# 필수는 앞쪽에, 옵션은 뒷쪽에 배치하는 것을 권고함
+def userURIBuilder(server,port,**user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL
+
+# 호출
+print(userURIBuilder("naver.com","80",id="kim",password="1234"))
+print(userURIBuilder("naver.com","80",id="kim",password="1234",name="mike",age="30"))
+
+# 람다 함수(간단하게 함수를 정의, 익명함수)
+g = lambda x,y:x*y
+print(g(3,4))
+print(g(5,6))
+
+# 메모리에 있는 변수, 함수를 딕셔너리 형태로 확인
+print(globals())
+
